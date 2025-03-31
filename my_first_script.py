c85f1b1892a6bdfce333c8dc13d557852c440af4
kim fines <kimfines@kims-MacBook-Pro.local>
2025-03-31 18:31:10 -0400

    adding my firsr script

---
+++ my_first_script.py
@@ -0,0 +1,32 @@
+# print ("Hello, World!")
+# # wax = "1 pound"                     
+# # # This variable is the weight of the wax.
+# # cost = 15
+
+# a= "bread"
+# b= "milk"
+# c= "eggs"
+# cost_eggs=3.50
+# cost_bread=4.00
+# cost_milk=4.99
+# total_cost= cost_bread+ cost_eggs+cost_milk
+# print(f"the total cost is {total_cost}")
+
+fav_store=input("what is your favorite store? ")
+print(f"welcome to {fav_store}")
+a="milk"
+b= "bread"
+cost_milk=3
+cost_bread=2.5
+total_cost= cost_milk +cost_bread
+print(f"the totalcost is{total_cost}")
+
+
+
+pwd
+git.clonegit@github.com:Kbeez2/ksgroceryapp.git
+
+
+
+git clone git@github.com:Kbeez2/ksgroceryapp.git cd ksgroceryapp
+
